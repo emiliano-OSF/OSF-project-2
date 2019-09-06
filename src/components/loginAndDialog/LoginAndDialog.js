5ae8fda7ed97362702cb04b02faaa41e73470a9e
Emiliano Feitosa <emiliano.feitosa@ifactory.com.br>
2019-09-06 12:36:33 -0300

    login component fully implemented

--- src/components/loginAndDialog/LoginAndDialog.js
+++ src/components/loginAndDialog/LoginAndDialog.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 import Modal from '@material-ui/core/Modal';
 
 import './LoginAndDialog.scss';
+import { tsImportEqualsDeclaration } from '@babel/types';
 
 
 export default class LoginAndDialog extends Component {
 
+    constructor() {
+        super();
+        this.showPassword = this.showPassword.bind(this);
+        this.validatePassword = this.validatePassword.bind(this)
+    }
+
     state = {
         showLogIn: false,
         password: '',
+        showPass: 'password',
+        okPassword: false,
         inputWarn: false,
-        warnings:[]
+        warnings: []
     }
 
     show() {
@@ -31,10 +40,23 @@ export default class LoginAndDialog extends Component {
         const validation = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})");
         if (validation.test(event.target.value)) {
             console.log("passou", event.target.value);
+            this.setState({
+                inputWarn: false,
+                okPassword: true
+            })
             return
         }
-        console.log("não passou", event.target.value)
+        console.log("não passou", event.target.value);
+        this.setState({
+            inputWarn: true,
+            okPassword: false
+        })
+    }
 
+    showPassword() {
+        this.setState({
+            showPass: this.state.showPass == "password" ? 'text' : 'password'
+        })
     }
 
     render() {
@@ -46,15 +68,22 @@ export default class LoginAndDialog extends Component {
                     <h3 >Sign in</h3>
                     <form className="login-dialog__form-group">
                         <div className="input-group">
-                            <label>Email</label>
-                            <input type='email' placeholder="Enter your email" onChange={this.validatePassword} />
+                            <label htmlFor="email">Email</label>
+                            <input id="email" type='email' placeholder="Enter your email" />
                         </div>
                         <div className="input-group">
-                            <label>Password <span>Forgot passowrd</span></label>
-                            <input type='password' placeholder="Enter your password" onFocus={()=>this.setState({inputWarn: true})}/>
-                            <small style={{ "display": this.state.inputWarn ? 'block' : 'none' }}>At least 6 characters length, 1 numeric, 1 uppercase letter and 1 special character</small>
-                            <small>
+                            <label htmlFor="password">Password <span>Forgot passowrd</span></label>
 
+                            <div className="password-field">
+                                <input id="password" type={this.state.showPass} placeholder="Enter your password" onChange={this.validatePassword} />
+                                <a className={`far fa-eye${this.state.showPass == "password" ? "" : "-slash"}`} onClick={this.showPassword}></a>
+                            </div>
+
+                            <small style={{ "display": this.state.inputWarn ? 'block' : 'none' }}>
+                                At least 6 characters length, 1 numeric, 1 uppercase letter and 1 special character
+                            </small>
+                            <small className="ok-password" style={{"display": this.state.okPassword? "block": "none"}}>
+                                Good password!
                             </small>
                         </div>
                         <button type='submit' >LOGIN</button>
