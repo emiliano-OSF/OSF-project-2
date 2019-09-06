af3b9cccf75ad3a493b15ce157d6573848620a4d
Emiliano Feitosa <emiliano.feitosa@ifactory.com.br>
2019-09-06 09:33:28 -0300

    category landing page fully created

--- src/components/categoryLandingPage/CategoryLandingPage.js
+++ src/components/categoryLandingPage/CategoryLandingPage.js
@@ -110,6 +110,9 @@ export default class CategoryLandingPage extends Component {
                     }
                 </div>
                 <button>Load More</button>
+                
+                
+
             </div >
         )
     }
