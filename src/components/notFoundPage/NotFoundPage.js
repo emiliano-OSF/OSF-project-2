f5e548f7fd8438f74a683b8f66548b99a14f503e
Emiliano Feitosa <emiliano.feitosa@ifactory.com.br>
2019-09-06 11:38:30 -0300

    not found page fully implemented

--- src/components/notFoundPage/NotFoundPage.js
+++ src/components/notFoundPage/NotFoundPage.js
@@ -2,13 +2,35 @@ import React, { Component } from 'react';
 import './NotFoundPage.scss';
 
 
-export default class NotFoudPage extends Component{
+export default class NotFoudPage extends Component {
 
-    render(){
-        return(
-            <h4>
-                Error 404: Not Found
-            </h4>
+    render() {
+        return (
+            <div className="not-found__container">
+                <h2 className='not-found__tittle'>404</h2>
+
+                <div className="not-found__panel">
+                    <div className="not-found__warn">
+                        <h1>Oops!</h1>
+                        <img src={require('../../assets/img/404/exclamation_mark.png')} />
+                    </div>
+                    <div className="not-found__info">
+                        <h5>Sorry, this page could not be found!</h5>
+                        <p>
+                            Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                            nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  Cum sociis natoque penatibus
+                            et. Lorem ipsum dolor sit amet.
+                        </p>
+                        <a href="/home">Go to Homepage</a>
+                        <h5>Search our site</h5>
+                        <div className="search-input">
+                            <input type="text" placeholder="SEARCH" />
+                            <i className="fas fa-search"></i>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         )
     }
 }
