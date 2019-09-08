e772cab3fecc2176baa5929e2743814d9895199e
emilianots <emiliano@alu.ufc.br>
2019-09-08 12:37:53 -0300

    minnor adjusts on homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.scss"
 
 class Footer extends Component {
 
-    state= {
+    state = {
         notFoundPage: '/home/404'
     }
 
@@ -18,7 +18,7 @@ class Footer extends Component {
                     </span>
                 </div>
 
-                <div className="footer-menu contact-sm">
+                <div className="footer-menu">
                     <div className="footer-menu--op contact-sm">
                         <span>Contact <span className="fas fa-caret-down"></span></span>
                         <p className="footer__contact">
@@ -29,7 +29,7 @@ class Footer extends Component {
                         </p>
                     </div>
                     <div className="footer-menu--op contact-md">
-                        <span>Cuuuuuuuuu <span className="fas fa-caret-down"></span></span>
+                        <span>Contact</span>
                         <p className="footer__contact">
                             Headquarters:
                             5600, Blvd. des Galeries, Bur 530 Québec, Québec G2K 2H6 <br />
@@ -37,7 +37,7 @@ class Footer extends Component {
                             <a href="tel:+1-888-548-4344">+1 (888) 548-4344</a>
                         </p>
                     </div>
-                    <div className="footer-menu--op">
+                    <div className="footer-menu--op footer-menu--sm">
                         <div className="footer-category">
                             <span className="footer-category">Categories <span className="fas fa-caret-down"></span></span>
                             <a className="footer-category__op" href={this.state.notFoundPage}>Accessories</a>
@@ -49,10 +49,43 @@ class Footer extends Component {
                             <a className="footer-category__op" href={this.state.notFoundPage}>Flowers & Plants</a>
                             <a className="footer-category__op" href={this.state.notFoundPage}>Food</a>
                         </div>
+                    </div><div className="footer-menu--op footer-menu--md">
+                        <div className="footer-category">
+                            <span className="footer-category">Categories </span>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Accessories</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Alcohol</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Art</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Books</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Drink</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Eletronics</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Flowers & Plants</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Food</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Eletronics</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Home</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Jewelry</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Kids & Baby</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Men's Fashion</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Mobile</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Motorcycles</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Movies</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Sports</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Toys</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Travel
+                            </a>
+                        </div>
                     </div>
-                    <div className="footer-menu--op">
+                    <div className="footer-menu--op footer-menu--sm">
                         <div className="footer-category">
                             <span className="footer-category">About <span className="fas fa-caret-down"></span></span>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>About us</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Delivery</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Testmonials</a>
+                            <a className="footer-category__op" href={this.state.notFoundPage}>Contact</a>
+
+                        </div>
+                    </div><div className="footer-menu--op footer-menu--md">
+                        <div className="footer-category">
+                            <span className="footer-category">About</span>
                             <a className="footer-category__op" href={this.state.notFoundPage}>About us</a>
                             <a className="footer-category__op" href={this.state.notFoundPage}>Delivery</a>
                             <a className="footer-category__op" href={this.state.notFoundPage}>Testmonials</a>
